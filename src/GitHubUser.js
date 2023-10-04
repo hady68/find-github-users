4c66e6fb2bda47ac5c83c27737ded827710bd259
Hardikk <kambojpushkal@gmail.com>
2023-10-04 14:09:12 +0530

    intial chnages

---
+++ src/GitHubUser.js
@@ -0,0 +1,53 @@
+import useGitHub from "./hooks/useGitHub";
+
+const GitHubUser = ({ username }) => {
+ 
+  const {user, loading, error} = useGitHub(username);
+
+    // Function to handle the button click and navigate to the user's profile
+    const handleViewProfileClick = () => {
+      if (user && user.html_url) {
+        window.open(user.html_url, "_blank"); // Open the profile in a new tab
+      }
+    };
+    
+  return (
+    <div className="github-user">
+      {loading && <p>Loading...</p>}
+      {error && <p>{error.message}</p>}
+
+      {user && (
+        <ul className="details">
+          <li>
+            <img src={user.avatar_url} alt={user.login} />
+          </li>
+          <li>
+            <strong>Name:</strong> {user.name}
+          </li>
+          <li>
+            <strong>Bio:</strong> {user.bio ? user.bio : "NA"}
+          </li>
+          <li>
+            <strong>Location:</strong> {user.location ? user.location : "NA"}
+          </li>
+          <li>
+            <strong>Blog or Site:</strong> {user.blog ? user.blog : "NA"}
+          </li>
+          <li>
+            <strong>Public Repos:</strong>{" "}
+            {user.public_repos ? user.public_repos : 0}
+          </li>
+          <li>
+            <strong>Followers:</strong> {user.followers ? user.followers : 0}
+          </li>
+          <li>
+            <strong>Following:</strong> {user.following ? user.following : 0}
+          </li>
+        </ul>
+        
+        )}
+        <button onClick={handleViewProfileClick}>Visit Profile</button>
+    </div>
+  );
+};
+export default GitHubUser;
